3a74b0acea3616a5c4b7566e161f8cb0be72d670
samera farah <samerafarah2@gmail.com>
2021-07-23 00:18:59 +0100

    lessons

---
+++ lessons.js
@@ -0,0 +1,72 @@
+    //array of lesson objects
+    lessons = [{
+        id: 0,
+        title: "Maths",
+        location: "Liverpool",
+        price: 250,
+        spaces: 5,
+        image: "images/maths.png"
+    }, {
+        id: 1,
+        title: "English Literature",
+        location: "Leeds",
+        price: 100,
+        spaces: 5,
+        image: ""
+    }, {
+        id: 2,
+        title: "Religous Education",
+        location: "Manchester",
+        price: 175,
+        spaces: 5,
+        image: "image/re.png"
+    }, {
+        id: 3,
+        title: "Chinese",
+        location: "Yorkshire",
+        price: 250,
+        spaces: 5,
+        image: ""
+    }, {
+        id: 4,
+        title: "Spanish",
+        location: "Birmingham",
+        price: 200,
+        spaces: 5,
+        image: ""
+    }, {
+        id: 5,
+        title: "Physical Education",
+        location: "Nottingham",
+        price: 100,
+        spaces: 5,
+        image: ""
+    }, {
+        id: 6,
+        title: "Music",
+        location: "Sheffield",
+        price: 390,
+        spaces: 5,
+        image: ""
+    }, {
+        id: 7,
+        title: "Science",
+        location: "Belfast",
+        price: 280,
+        spaces: 5,
+        image: ""
+    }, {
+        id: 8,
+        title: "Geography",
+        location: "Edinborough",
+        price: 150,
+        spaces: 5,
+        image: ""
+    }, {
+        id: 9,
+        title: "History",
+        location: "Wales",
+        price: 200,
+        spaces: 5,
+        image: ""
+    }]
